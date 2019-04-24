3d3c5c679fae1b73b413f973cfef6697b9d6933f
Thithran <thi.tran.fi@gmail.com>
2019-04-24 17:24:38 +0300

    adding from fb

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
-const express = require('express');
-const bodyParser  = require('body-parser');
-const request = require('request');
+// Imports dependencies and set up http server
+const
+  express = require('express'),
+  bodyParser = require('body-parser'),
+  app = express().use(bodyParser.json()); // creates express http server
 
-// hosting set up 
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-
-app.listen(port, () => {
-    console.log('Server is up on port ' + port);
-})
+// Sets server port and logs message on success
+app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
 app.get('/', (req,res) => {
     res.send("Hi I am a chatbot");
@@ -19,10 +13,57 @@ app.get('/', (req,res) => {
 
 // Facebook
 
-app.get('/webhook/', (req,res) => {
-    if (req.query['hub.verify_token'] === "thitran") {
-        res.send(req.query['hub.challenge'])
-    } 
-    res.send("Wrong token")
-})
 
+
+// Creates the endpoint for our webhook 
+app.post('/webhook', (req, res) => {  
+ 
+    let body = req.body;
+  
+    // Checks this is an event from a page subscription
+    if (body.object === 'page') {
+  
+      // Iterates over each entry - there may be multiple if batched
+      body.entry.forEach(function(entry) {
+  
+        // Gets the message. entry.messaging is an array, but 
+        // will only ever contain one message, so we get index 0
+        let webhook_event = entry.messaging[0];
+        console.log(webhook_event);
+      });
+  
+      // Returns a '200 OK' response to all requests
+      res.status(200).send('EVENT_RECEIVED');
+    } else {
+      // Returns a '404 Not Found' if event is not from a page subscription
+      res.sendStatus(404);
+    }
+  
+  });
+
+  app.get('/webhook', (req, res) => {
+
+    // Your verify token. Should be a random string.
+    let VERIFY_TOKEN = "thitran"
+      
+    // Parse the query params
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+      
+    // Checks if a token and mode is in the query string of the request
+    if (mode && token) {
+    
+      // Checks the mode and token sent is correct
+      if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+        
+        // Responds with the challenge token from the request
+        console.log('WEBHOOK_VERIFIED');
+        res.status(200).send(challenge);
+      
+      } else {
+        // Responds with '403 Forbidden' if verify tokens do not match
+        res.sendStatus(403);      
+      }
+    }
+  });
