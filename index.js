5a0528e8bf7e99bd201466fe5da19401ae2c5fb9
Thithran <thi.tran.fi@gmail.com>
2019-04-24 18:47:45 +0300

    adding small fix

--- index.js
+++ index.js
@@ -1,76 +1,66 @@
-// Imports dependencies and set up http server
-const
-  express = require('express'),
-  bodyParser = require('body-parser'),
-  app = express().use(bodyParser.json()); // creates express http server
-const request = require('request');
-// Sets server port and logs message on success
-app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
 
-app.get('/', (req,res) => {
-    res.send("Hi I am a chatbot");
-});
+const app = express()
 
-// Facebook
+app.set('port', (process.env.PORT || 5000))
 
-const token = "EAAelKjl2oDgBANHurTAbqZC4rjzeXVAqigPTErZCaMY4hiD3KqpM2urysjLQSZChqHnswpgTcVq9u5Sjt3XwsB6dzGZBSq5EzzZAvMjiKGnt5BvIrcZATZCc1Y8XiLBnwgQFlZCUIZByArIVqFNijFDWWve6jeUiH8xrthJMS13DNjwLd3vN94Mhm";
+// Allows us to process the data
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
-// Creates the endpoint for our webhook 
-app.post('/webhook/', (req, res) => {  
-    let messaging_events = req.body.entry[0].messaging;
-    for (let i = 0; i< messaging_events.lenth; i++) {
-        let event = messaging_events[i];
-        let sender = event.sender.id;
-        if (event.message && event.message.text) {
-            let text = event.message.text;
-            sendText(sender, "Text echo: "+ text.substring(0,100));
-        }
-    }
-    res.status(200).send('request sent')
-});
+// ROUTES
 
-sendText = (sender, text) => {
-    let messageData = {text};
-    request({
-        url: "https://graph.facebook.com/v2.6/me/messages",
-        qs: {access_token: token},
-        method: "POST",
-        json: {
-            receipient: {id: sender},
-            message: messageData
-        }
-    }, (err, response, body) => {
-        if (err) {
-            console.log("sending erro");
-        } else if (response.body.error) {
-            console.log("Resposne body error")
-        }
-    })
+app.get('/', function(req, res) {
+	res.send("Hi I am a chatbot")
+})
+
+let token = "EAAelKjl2oDgBANHurTAbqZC4rjzeXVAqigPTErZCaMY4hiD3KqpM2urysjLQSZChqHnswpgTcVq9u5Sjt3XwsB6dzGZBSq5EzzZAvMjiKGnt5BvIrcZATZCc1Y8XiLBnwgQFlZCUIZByArIVqFNijFDWWve6jeUiH8xrthJMS13DNjwLd3vN94Mhm";
+
+// Facebook 
+
+app.get('/webhook/', function(req, res) {
+	if (req.query['hub.verify_token'] === "blondiebytes") {
+		res.send(req.query['hub.challenge'])
+	}
+	res.send("Wrong token")
+})
+
+app.post('/webhook/', function(req, res) {
+	let messaging_events = req.body.entry[0].messaging
+	for (let i = 0; i < messaging_events.length; i++) {
+		let event = messaging_events[i]
+		let sender = event.sender.id
+		if (event.message && event.message.text) {
+			let text = event.message.text
+			sendText(sender, "Text echo: " + text.substring(0, 100))
+		}
+	}
+	res.sendStatus(200)
+})
+
+function sendText(sender, text) {
+	let messageData = {text: text}
+	request({
+		url: "https://graph.facebook.com/v2.6/me/messages",
+		qs : {access_token: token},
+		method: "POST",
+		json: {
+			recipient: {id: sender},
+			message : messageData,
+		}
+	}, function(error, response, body) {
+		if (error) {
+			console.log("sending error")
+		} else if (response.body.error) {
+			console.log("response body error")
+		}
+	})
 }
 
-  app.get('/webhook/', (req, res) => {
+app.listen(app.get('port'), function() {
+	console.log("running: port")
+})
 
-    // Your verify token. Should be a random string.
-    let VERIFY_TOKEN = "thitran"
-      
-    // Parse the query params
-    let mode = req.query['hub.mode'];
-    let token = req.query['hub.verify_token'];
-    let challenge = req.query['hub.challenge'];
-      
-    // Checks if a token and mode is in the query string of the request
-    if (mode && token) {
-    
-      // Checks the mode and token sent is correct
-      if (token === VERIFY_TOKEN) {
-        
-        // Responds with the challenge token from the request
-        console.log('WEBHOOK_VERIFIED');
-        res.status(200).send(challenge);
-      
-      } else {
-        // Responds with '403 Forbidden' if verify tokens do not match
-        res.sendStatus(403);      
-      }
-    }
-  });
+
